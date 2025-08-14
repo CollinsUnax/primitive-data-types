47a56533cefb992e0fb229a761d0e7405409d2a9
Collins Unachukwu <collinsunax99@gmail.com>
2025-08-14 13:03:47 +0100

    Completed javascript booleans exercises

--- booleans/booleans.js
+++ booleans/booleans.js
@@ -6,7 +6,7 @@
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let newBoolean
+let newBoolean= true
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -25,9 +25,9 @@ let dadsHeight = generateRandomNumber(100, 200)
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let momIsTaller
-let dadIsTaller
-let areTheSameHeight
+let momIsTaller= momsHeight > dadsHeight
+let dadIsTaller= dadsHeight > momsHeight
+let areTheSameHeight= momsHeight === dadsHeight
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -46,7 +46,7 @@ let studentEssay = generateSudentEssay()
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let notPlagiarised
+let notPlagiarised= !studentEssay.includes(keyPhrase)
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -68,10 +68,10 @@ let thresholdForTall = 150
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let siblingsAreTall
-let siblingsAreShort
-let atLeastOneIsTall
-let onlyOneIsTall
+let siblingsAreTall= sistersHeight > thresholdForTall && brothersHeight > thresholdForTall
+let siblingsAreShort= sistersHeight < thresholdForTall && brothersHeight < thresholdForTall
+let atLeastOneIsTall= sistersHeight > thresholdForTall || brothersHeight > thresholdForTall
+let onlyOneIsTall= (sistersHeight > thresholdForTall) !== (brothersHeight > thresholdForTall)
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -94,7 +94,10 @@ let secretCode = generateSecretCode()
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let codeIsValid
+let codeIsValid=
+secretCode.length=== 15 &&
+secretCode[4] >= 'A' && secretCode[4] <= 'Z' &&
+!isNaN(parseInt(secretCode[9], 10))
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -115,13 +118,13 @@ let truthy1, truthy2
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-// falsy1 = Boolean("")
-// falsy2 = Boolean(0)
-// falsy3 = Boolean(undefined)
-// falsy4 = Boolean(null)
+falsy1 = Boolean("")
+falsy2 = Boolean(0)
+falsy3 = Boolean(undefined)
+falsy4 = Boolean(null)
 
-// truthy1 = Boolean("non-empty string")
-// truthy2 = Boolean(1)
+truthy1 = Boolean("non-empty string")
+truthy2 = Boolean(1)
 
 ////////////////////////////////////////////////////////////////////////
 
